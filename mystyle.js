e4a53f4e4e4c1cbb46b8e0fd68c212fa859b4773
BoiledFijiWaer <42652947+BoilFijiWater@users.noreply.github.com>
2018-09-09 20:35:48 -0700

    Update mystyle.js

--- mystyle.js
+++ mystyle.js
@@ -1,3 +1,8 @@
- $("button").click(function(){
-    
+ $("#p1").click(function(){
+    $("#p2").show()
+    $("#p3").show()
+    $("#p4").show()
+    $("#p5").show()
+    $("#p6").show()
+    $("#p7").show()
 });
