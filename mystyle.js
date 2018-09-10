c746b6c1708f8ba53d5ca3b0cb3d01044ba53a51
BoiledFijiWaer <42652947+BoilFijiWater@users.noreply.github.com>
2018-09-10 10:43:07 -0700

    Update mystyle.js

--- mystyle.js
+++ mystyle.js
@@ -1,23 +1,23 @@
-
-$("#p1").click(function(){
-    $("#p2").toggle()
-    $("#p3").toggle()
-    $("#p4").toggle()
-    $("#p5").toggle()
-    $("#p6").toggle()
-    $("#p7").toggle()
-    $("#p8").toggle()
-    $("#answers").toggle()
-});
-
-                   
- $("#answers").click(function(){
-    $("#p9").toggle()
-    $("#p10").toggle()
-    $("#p11").toggle()
-    $("#p12").toggle()
-    $("#p13").toggle()
-    $("#p14").toggle()
-    $("#p15").toggle()
- })
-
+$(document).ready(function(){
+    $("#p1").click(function(){
+        $("#p2").toggle()
+        $("#p3").toggle()
+        $("#p4").toggle()
+        $("#p5").toggle()
+        $("#p6").toggle()
+        $("#p7").toggle()
+        $("#p8").toggle()
+        $("#answers").toggle()
+    });
+})
+$(document).ready(function(){        
+     $("#answers").click(function(){
+        $("#p9").toggle()
+        $("#p10").toggle()
+        $("#p11").toggle()
+        $("#p12").toggle()
+        $("#p13").toggle()
+        $("#p14").toggle()
+        $("#p15").toggle()
+     })
+}) 
